a656cda8793bc8585e77bf65dffadf0dbd4c9103
quezia-lima <qfonseca1@gmail.com>
2022-05-31 22:41:36 -0300

    import header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 import Table from './components/Table';
 import SearchBar from './components/SearchBar';
 import NumberFilter from './components/NumberFilter';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <PlanetsProvider>
+      <Header />
       <SearchBar />
       <NumberFilter />
       <Table />
