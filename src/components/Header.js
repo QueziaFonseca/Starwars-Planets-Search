7ed21a9aa25661bf14f1ed1f612e7e3e6a38b267
quezia-lima <qfonseca1@gmail.com>
2022-05-31 22:39:05 -0300

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div className="header-title">
+        <h1>Projeto Star Wars - Trybe</h1>
+        <div className="empty" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
